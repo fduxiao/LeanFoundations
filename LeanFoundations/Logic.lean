1aecc82f1a2e6d04cd9ac36198f41c310810ec8d
fduxiao <9294979+fduxiao@users.noreply.github.com>
2025-04-30 18:13:18 -0400

    sketch of Logic Foundations

--- LeanFoundations/Logic.lean
+++ LeanFoundations/Logic.lean
@@ -11,6 +11,18 @@ import LeanFoundations.Logic.Basic
 /-!
 # Contents
 - `LeanFoundations.Logic.Basic`: Functional programming in Lean
+- `LeanFoundations.Logic.Induction`: Proof by induction
+- Structured data
+- Polymorphism
+- Tactics and logics: I tend to merge them together becasue tactics are specific to logics
+- Inductively defined propositions
+- Curry-Howard correspondence.
+- Inductive principles
+- Relations and typeclasses
+- Make a small DSL for arithmetic and compile it to a stack calculator
+- Monad and IO and generate some json representing the stack calculator
+- Meta-programming: notation, syntax, macro, elaborations.
+- Make our own tactics
 -/
 
 
---
+++ LeanFoundations/Logic/Induction.lean
@@ -0,0 +1,11 @@
+/-
+Copyright (c) 2025 Xiao Tan. All rights reserved.
+Released under MIT license as described in the file LICENSE.
+Authors: Xiao Tan
+
+! Upstream reference.
+-/
+
+/-!
+# Proof by introduction
+-/
