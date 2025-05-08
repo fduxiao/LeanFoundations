76f0165d0d1fe329b5a383a9189a0a2a613723b5
fduxiao <9294979+fduxiao@users.noreply.github.com>
2025-05-08 18:25:30 -0400

    add preface

--- LeanFoundations.lean
+++ LeanFoundations.lean
@@ -5,12 +5,33 @@ Authors: Xiao Tan
 
 -/
 
--- This module serves as the root of the `LeanFoundations` library.
--- Import modules here that should be built as part of the library.
 import LeanFoundations.Logic
 import LeanFoundations.PL
 
 
+/-!
+# Preface
+We are inspired by the [Software Foundations (SF)][Pierce:SF1]
+series. There should also be a systematic introduction to `how to
+programming and verify algorithms in lean`. Yet, we are more ambitious
+in that we want to show how to make a (toy) lean out of lean itself and
+verify related code. We assume you know nothing about computer programming,
+but we do assume you are familiar with Unix-like operating systems
+(shell commands, text editors, file systems, etc) so that you know how to
+install `lean`, write the code and run other tools like `lake` or `elan`.
+If you are taking/teaching a course, then the first class should be about
+how to use a Unix-like operating system, system-wide software package
+management and install `lean`. And, some kind of proficiency of mathematics
+is helpful (and maybe necessary, e.g., you have to know natural numbers and
+mathematical induction on them).
+
+The whole book is built on
+[this Github repository](https://github.com/fduxiao/LeanFoundations).
+It is just an ordinary `lean` package where the `module comments` are compiled
+as descriptions in this book, while the lean codes are displayed with
+a light green background. You can clone the repository or just make your
+own `lake project` and copy those lines.
+-/
 
 /-TOC-/
 /-!
@@ -18,7 +39,6 @@ import LeanFoundations.PL
 - `LeanFoundations.Logic`: Logical foundations in Lean
 - `LeanFoundations.PL`: Programming language foundations in Lean
 -/
-
 
 /-!
 # QuickStart
